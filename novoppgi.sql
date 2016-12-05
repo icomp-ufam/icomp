a8f1278d8ba7b9190b6c89b18b30bca07f88c634
Pedro Frota <pvmf@icomp.ufam.edu.br>
2016-12-05 04:28:20 -0400

    Correção na atualização no DUMP do banco

--- novoppgi.sql
+++ novoppgi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 03-Dez-2016 às 18:25
+-- Generation Time: 05-Dez-2016 às 04:23
 -- Versão do servidor: 5.5.53-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.20
 
@@ -2070,7 +2070,8 @@ INSERT INTO `j17_contproj_projetos` (`id`, `nomeprojeto`, `orcamento`, `saldo`, 
 (76, 'Melhorando a Qualidade no Desenvolvimento de Software - PROTI PESQUISA', 240882.51, 108709.24, '2014-03-01', '2014-09-30', '0000-00-00', 30, 1, 48, '5786-X', '285-2', '', 'components/com_controleprojetos/docprojetos/proposta/PPGI-Proposta.pdf', 'Encerrado'),
 (83, 'eVox-Mobilidade - PROTI MOBILIDADE', 50092, 20937.47, '2013-09-30', '2015-03-29', '0000-00-00', 10, 1, 48, '5786-X', '277-1', '', '', 'Encerrado'),
 (84, 'ACAMPAR - PROTI - MOBILIDADE', 109263, 22604.33, '2014-09-30', '2015-10-30', '0000-00-00', 46, 1, 48, '5786-x', '296-8', 'components/com_controleprojetos/docprojetos/edital/PPGI-Edital-de19dc7cd360579612010bb4605fe87b.pdf', 'components/com_controleprojetos/docprojetos/proposta/PPGI-Proposta-de19dc7cd360579612010bb4605fe87b.pdf', 'Encerrado'),
-(85, 'Monitoramento e Controle de Redes - PROTI MOBILIDADE', 75252, 31696.13, '2014-09-30', '2015-11-30', '0000-00-00', 15, 1, 48, '5786-X', '273-9', 'components/com_controleprojetos/docprojetos/edital/PPGI-Edital-fac97008713a3bcd93358ea2f19618a2.pdf', '', 'Encerrado');
+(85, 'Monitoramento e Controle de Redes - PROTI MOBILIDADE', 75252, 31696.13, '2014-09-30', '2015-11-30', '0000-00-00', 15, 1, 48, '5786-X', '273-9', 'components/com_controleprojetos/docprojetos/edital/PPGI-Edital-fac97008713a3bcd93358ea2f19618a2.pdf', '', 'Encerrado'),
+(86, '1111', 11.11, 0, '2016-12-28', '2016-12-29', '2016-12-29', 68, 6, 8, 'asdas', 'aasas', '', '', 'Cadastrado');
 
 -- --------------------------------------------------------
 
@@ -4354,12 +4355,11 @@ INSERT INTO `j17_projetos` (`id`, `idProfessor`, `titulo`, `descricao`, `inicio`
 CREATE TABLE `j17_prorrogacoes` (
   `id` int(11) NOT NULL,
   `idAluno` int(11) NOT NULL,
+  `dataSolicitacao` date NOT NULL,
+  `qtdDias` int(11) NOT NULL,
   `justificativa` text NOT NULL,
   `previa` text NOT NULL,
-  `dataSolicitacao` date NOT NULL,
-  `dataAprovOrientador` date NOT NULL,
-  `dataAprovColegiado` date NOT NULL,
-  `status` smallint(6) NOT NULL
+  `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -5904,22 +5904,11 @@ CREATE TABLE `j17_trancamentos` (
   `dataSolicitacao` date NOT NULL,
   `dataInicio` date NOT NULL,
   `prevTermino` date NOT NULL,
-  `dataTermino` date NOT NULL,
+  `dataTermino` date DEFAULT NULL,
   `justificativa` varchar(250) NOT NULL,
   `documento` text NOT NULL,
   `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `j17_trancamentos`
---
-
-INSERT INTO `j17_trancamentos` (`id`, `idAluno`, `tipo`, `dataSolicitacao`, `dataInicio`, `prevTermino`, `dataTermino`, `justificativa`, `documento`, `status`) VALUES
-(1, 713, 0, '2016-12-01', '2016-12-16', '0000-00-00', '0000-00-00', 'oijni', 'uploads/trancamento-W7htRcwLzBNOBR9AaWfv-KHExWI_PcoB.pdf', 1),
-(2, 713, 0, '2016-12-03', '2016-12-17', '0000-00-00', '0000-00-00', 'xdvdfd', 'uploads/trancamento-8vUxxZa7VdTw8FNFXpeLVX0lWzB28Z4z.pdf', 0),
-(3, 714, 0, '2016-12-02', '2016-12-18', '0000-00-00', '0000-00-00', 'aaaasa', 'uploads/trancamento-6s25dUbcV9Y-KVqqb0dlduD2hAPXHDAJ.pdf', 0);
-
--- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `j17_user`
@@ -5963,6 +5952,8 @@ CREATE TABLE `j17_user` (
   `idRH` int(11) DEFAULT NULL,
   `cargo` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `migration`
@@ -6259,7 +6250,7 @@ ALTER TABLE `j17_contproj_despesas`
 -- AUTO_INCREMENT for table `j17_contproj_projetos`
 --
 ALTER TABLE `j17_contproj_projetos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 --
 -- AUTO_INCREMENT for table `j17_contproj_prorrogacoes`
 --
@@ -6349,12 +6340,12 @@ ALTER TABLE `j17_reservas_salas`
 -- AUTO_INCREMENT for table `j17_trancamentos`
 --
 ALTER TABLE `j17_trancamentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 --
 -- AUTO_INCREMENT for table `j17_user`
 --
 ALTER TABLE `j17_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 --
 -- Constraints for dumped tables
 --
