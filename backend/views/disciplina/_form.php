c6d200cb7063c475e36f1aa5638824c0be0deba7
malbicomp <malb@icomp.ufam.edu.br>
2016-12-14 18:23:05 -0400

    merge Manual branch aproveitamentoDisciplina com o master

--- backend/views/disciplina/_form.php
+++ backend/views/disciplina/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\widgets\MaskedInput;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Disciplina */
@@ -11,19 +12,30 @@ use yii\widgets\ActiveForm;
 <div class="disciplina-form">
 
     <?php $form = ActiveForm::begin(); ?>
+<div class="panel panel-default" style="width:80%">
+	 <div class="panel-heading">
+      <h3 class="panel-title"><b>Dados da Disciplina:</b></h3>
+     </div>
+	  <div class="panel-body">
 
-    <?= $form->field($model, 'codDisciplina')->textInput(['maxlength' => true, 'style'=>['width'=>'250px']]) ?>
+		<div class="row">
+    <?= $form->field($model, 'codDisciplina', ['options'=>['class'=>'col-md-3']])->textInput(['maxlength' => true]) ?>
+		</div>
+		<div class="row">
+    <?= $form->field($model, 'nome', ['options'=>['class'=>'col-md-5']])->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'nome')->textInput(['maxlength' => true, 'style'=>['width'=>'250px']]) ?>
-<div style="display:inline">
-    <?= $form->field($model, 'creditos')->textInput(['style'=>['width'=>'50px', 'display'=>'inline','margin-left'=>'32px']]) ?>
-
-    <?= $form->field($model, 'cargaHoraria')->textInput(['style'=>['width'=>'50px','display'=>'inline']]) ?>
-</div>
+		</div>
+		<div class="row">
+    <?= $form->field($model, 'creditos',['options'=>['class'=>'col-md-2']])->textInput()->widget(MaskedInput::className(), [
+                    'mask' => '99', 'clientOptions'=>['alias'=>'numeric']]) ?>
+    <?= $form->field($model, 'cargaHoraria',['options'=>['class'=>'col-md-3']])->textInput()->widget(MaskedInput::className(), [
+                    'mask' => '999', 'clientOptions'=>['alias'=>'numeric']]) ?>
+        </div>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Salvar' : 'Alterar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
-
+   </div>
+</div>
     <?php ActiveForm::end(); ?>
 
 </div>
