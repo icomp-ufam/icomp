f95b82659c4f484330ee3d9ce84745a0bfda904b
malbicomp <malb@icomp.ufam.edu.br>
2017-01-25 18:21:42 -0400

    correção de namespace.

--- backend/models/EquipamentoSearch.php
+++ backend/models/EquipamentoSearch.php
@@ -5,7 +5,7 @@ namespace app\models;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\models\Equipamento;
+use backend\models\Equipamento;
 
 /**
  * EquipamentoSearch represents the model behind the search form of `app\models\Equipamento`.
