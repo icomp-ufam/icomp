22909a8a235d1c64c92d56adffbca7e6bbe6075c
malbicomp <malb@icomp.ufam.edu.br>
2017-01-30 20:44:36 -0400

    correcao do formulario.

--- backend/views/cautela/_form.php
+++ backend/views/cautela/_form.php
@@ -30,10 +30,44 @@ $this->registerJs(
 		);
 ?>
     <?php $form = ActiveForm::begin(); ?>
+    
+	<div class="panel panel-default" style="width:80%">
+	
+	<div class="panel-heading">
+    	<h3 class="panel-title"><b>Dados Respons&aacute;vel:</b></h3>
+    </div>
+	<div class="panel-body">    
+	<div class="row">
+    <?= $form->field($model, 'NomeResponsavel', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="row">
+    <?= $form->field($model, 'TelefoneResponsavel', ['options' => ['class' => 'col-md-3']])->widget(MaskedInput::className(), [
+                            'mask' => '(99) 99999-9999'])->label("<b>Telefone:</b>") ?>
+    
+    <?= $form->field($model, 'Email',['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true]) ?>
+    </div>
+    </div>
+    <div class="panel-heading">
+    	<h3 class="panel-title"><b>Dados Equipamento:</b></h3>
+    </div>
+    <div class="panel-body"> 
+    <div class="row">
+    <?= $form->field($model, 'Equipamento', ['options' => ['class' => 'col-md-3']])->dropDownList(ArrayHelper::map(backend\models\Equipamento::find()->all(),'idEquipamento','NomeEquipamento')) ?>
+    </div>
+    <div class="row">
+    <?= $form->field($model, 'OrigemCautela', ['options' => ['class' => 'col-md-3']])->textInput(['maxlength' => true]) ?>    
+    <?= $form->field($model, 'idProjeto', ['options' => ['class' => 'col-md-3']])->dropDownList([ArrayHelper::map(backend\models\ContProjProjetos::find()->all(),'id','nomeprojeto')]) ?>
+	</div>
+   </div>
+     <div class="panel-heading">
+    	<h3 class="panel-title"><b>Dados Cautela:</b></h3>
+    </div>
+    <div class="panel-body">
+	<div class="row">
+    <?= $form->field($model, 'StatusCautela', ['options' => ['class' => 'col-md-3']])->dropDownList(['Em aberto' => 'Em aberto','Concluída'=> 'Concluída','Em atraso'=>'Em atraso']) ?>
+   	</div>
+	
 
-    <?= $form->field($model, 'NomeResponsavel')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'OrigemCautela')->textInput(['maxlength' => true]) ?>
 
     <div class="row">
                 <?= $form->field($model, 'DataDevolucao', ['options' => ['class' => 'col-md-4']])->widget(DatePicker::classname(), [
@@ -47,8 +81,6 @@ $this->registerJs(
                 ?>
     </div>
 
-    <?= $form->field($model, 'Email')->textInput(['maxlength' => true]) ?>
-
      <div class="row">
                 <?= $form->field($model, 'ValidadeCautela', ['options' => ['class' => 'col-md-4']])->widget(DatePicker::classname(), [
                     'language' => 'pt-BR',
@@ -61,28 +93,17 @@ $this->registerJs(
                 ?>
     </div>
 
-    <div class="row">
-    <?= $form->field($model, 'TelefoneResponsavel', ['options' => ['class' => 'col-md-3']])->widget(MaskedInput::className(), [
-                            'mask' => '(99) 99999-9999'])->label("<b>Telefone:</b>") ?>
-    </div>
-
-   
-
-   <?= $form->field($model, 'Equipamento')->dropDownList(ArrayHelper::map(backend\models\Equipamento::find()->all(),'idEquipamento','NomeEquipamento')) ?>
-
-	<?= $form->field($model, 'idEquipamento')->hiddenInput(['value'=>$model->Equipamento])->label(false) ?>
-
-    <?= $form->field($model, 'StatusCautela')->dropDownList(['Em aberto' => 'Em aberto','Concluída'=> 'Concluída','Em atraso'=>'Em atraso']) ?>
-
-    
-
-    <?= $form->field($model, 'idProjeto')->dropDownList([ArrayHelper::map(backend\models\ContProjProjetos::find()->all(),'id','nomeprojeto')]) ?>
-
+	<div class="row">    
+    <?= $form->field($model, 'ImagemCautela',['options'=>['class'=>'col-md-3']])->fileInput(['maxlength' => true]) ?>
+	</div>
+	</div>
+	</div>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Salvar' : 'Atualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
         <?= Html::a('Cancelar', ['index'], ['class' => 'btn btn-danger']) ?>
     </div>
-
+    
+	<?= $form->field($model, 'idEquipamento')->hiddenInput(['value'=>$model->Equipamento])->label(false) ?>
     <?php ActiveForm::end(); ?>
 
 </div>
